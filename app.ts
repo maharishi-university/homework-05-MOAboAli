4cd73acf956d055de36fe14f57a6297529f057a1
MoAboali <mohamed.aboali20@outlook.com>
2024-07-01 14:46:28 -0500

    Last Modify

--- app.ts
+++ app.ts
@@ -3,16 +3,16 @@ import { isPrime } from './Ex2.ts';
 import { getData } from './Ex.3.ts';
 
 
-// //Ex.1
-// ShowResult();
+//Ex.1
+ShowResult();
 
 
-// //Ex.2
-// console.log('start');
-// isPrime(7)
-//     .then(console.log)
-//     .catch(console.error);
-// console.log('end');
+//Ex.2
+console.log('start');
+isPrime(7)
+    .then(console.log)
+    .catch(console.error);
+console.log('end');
 
 
 //Ex.3
